c46e5129cee99587903ff26eac940e8ee72e3855
EricDeveloper1 <128795646+EricDeveloper1@users.noreply.github.com>
2023-08-27 18:46:35 +0400

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAWMeI54v_f8bM3Uw6lOyjCHEEciXTLkmQ",
+    authDomain: "project43-57fdd.firebaseapp.com",
+    databaseURL: "https://project43-57fdd-default-rtdb.firebaseio.com",
+    projectId: "project43-57fdd",
+    storageBucket: "project43-57fdd.appspot.com",
+    messagingSenderId: "332716865046",
+    appId: "1:332716865046:web:86447bb8ff17b627482737",
+    measurementId: "G-171YZHPPVS"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
